b14ee9aa3f335ce6816153e302e45cfb803efacd
wgraham <Will Graham>
2015-03-14 20:54:53 -0400

    Initial commit of source.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # skymod-startingmoney
+
 Gives you $500,000 instead of $70,000 at the start of a city to move quicker and shorten the beginning phase of building the city.
+
+## How to Build
+
+1. Clone the source.
+2. Open in Visual Studio (tested on 2013).
+3. Add the game assembly path to the [project reference paths](https://msdn.microsoft.com/en-us/library/vstudio/6taasyc6(v=vs.100).aspx).
+  1. The path is `[SteamLibrary]\steamapps\common\Cities_Skylines\Cities_Data\Managed`, where `[SteamLibrary]` is your Steam library folder.
+4. Build.
+
+## What if I just want the mod to use it?
+
+It's on the workshop! Head over to http://steamcommunity.com/sharedfiles/filedetails/?id=408051079
---
+++ src/SkyModStartingMoney/SkyModStartingMoney/StartingMoneyMod.cs
@@ -0,0 +1,43 @@
+﻿namespace SkyModStartingMoney
+{
+    using ICities;
+    using System;
+    using System.Threading;
+    using UnityEngine;
+
+    public class StartingMoneyMod : IUserMod
+    {
+        public string Description
+        {
+            get { return "Gives you $500,000 starting cash on new cities."; }
+        }
+
+        public string Name
+        {
+            get { return "SkyMod Start Money"; }
+        }
+    }
+
+    public class StartingMoneyLoadingExtension : LoadingExtensionBase
+    {
+        public override void OnLevelLoaded(LoadMode mode)
+        {
+            if (mode == LoadMode.NewGame)
+            {
+                try
+                {
+                    var type = typeof(EconomyManager);
+                    var cashAmountField = type.GetField("m_cashAmount", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
+
+                    cashAmountField.SetValue(EconomyManager.instance, 50000000);
+                }
+                catch (Exception ex)
+                {
+                    // Gulp
+                }
+            }
+
+            base.OnLevelLoaded(mode);
+        }
+    }
+}
